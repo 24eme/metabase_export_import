13616ed8cb608686eff5c2b51e9e898ba25684f3
Tangui Morlier <tangui@tangui.eu.org>
2021-08-06 15:52:33 +0200

    Export + base en parametre

--- metabase_api.py
+++ metabase_api.py
@@ -6,16 +6,19 @@ import csv
 metabase_apiurl = sys.argv[1]
 metabase_username = sys.argv[2]
 metabase_password = sys.argv[3]
+metabase_base = sys.argv[4]
 #metabase_apiurl = "http://localhost/api/"
 
 class Metabase:
-    def __init__(self, apiurl, username, password):
+    def __init__(self, apiurl, username, password, debug=False):
         self.apiurl = apiurl
         self.username = username
         self.password = password
+        self.debug = debug
+        
         self.metabase_session = None
-        self.debug = False
-
+        self.database_export = None
+        
     def query (self, method, query_name, json_data = None):
         json_str = None
         if json_data is not None:
@@ -41,6 +44,12 @@ class Metabase:
                              )
         elif method == 'GET':
             r = requests.get(
+                                query_url,
+                                data = json_str,
+                                headers= headers
+                            )        
+        elif method == 'PUT':
+            r = requests.put(
                                 query_url,
                                 data = json_str,
                                 headers= headers
@@ -152,45 +161,96 @@ class Metabase:
         self.query('DELETE', 'session', {'metabase-session-id': self.metabase_session})
         self.metabase_session = None
 
-    def fieldid2tableandfield(self, database_export, field_id):
+    def field_id2tablenameandfieldname(self, database_name, field_id):
+        if self.database_export is None:
+            self.database_export = self.get_database(database_name, True)
         if not field_id:
             return ['', '']
-        for table in database_export['tables']:
+        for table in self.database_export['tables']:
             for field in table['fields']:
                 if field['id'] == field_id:
                     return [table['name'], field['name']]
         return ['', '']
 
+    def field_tablenameandfieldname2field(self, database_name, table_name, field_name):
+        if self.database_export is None:
+            self.database_export = self.get_database(database_name, True)
+        if not table_name or not field_name:
+            return None
+        for table in self.database_export['tables']:
+            if table['name'] == table_name:
+                for field in table['fields']:
+                    if field['name'] == field_name:
+                        return field
+        return None
+                    
+                    
+
     def export_fields(self, database_name):
-        database_export = self.get_database(database_name, True)
+        self.database_export = self.get_database(database_name, True)
         result = []
-        for table in database_export['tables']:
+        for table in self.database_export['tables']:
             table_name = table['name']
             for field in table['fields']:
                 field_id = field['fk_target_field_id']
-                [fk_table, fk_field] = self.fieldid2tableandfield(database_export, field_id)
+                [fk_table, fk_field] = self.field_id2tablenameandfieldname(database_name, field_id)
                 result.append({
-                                'database_name': database_name, 'table_name': table_name,
-                                'field_name': field['name'], 'semantic_type': str(field['semantic_type']),
+                                'table_name': table_name, 'field_name': field['name'], 'description': field['description'],
+                                'semantic_type': str(field['semantic_type']),
                                 'foreign_table': fk_table, 'foreign_field': fk_field,
                                 'visibility_type': field['visibility_type'], 'has_field_values': field['has_field_values'],
                                 'custom_position': str(field['custom_position']), 'effective_type': field['effective_type'],
                                 'base_type': field['base_type'], 'database_type': field['database_type']
                               })
         return result
+        
+    def export_fields_to_csv(self, database_name, filename):
+        export = metabase.export_fields(database_name)
+        with open(filename, 'w', newline = '') as csvfile:
+            my_writer = csv.writer(csvfile, delimiter = ',')
+            need_header = True
+            for row in export:
+                if need_header:
+                    my_writer.writerow(row.keys())
+                    need_header = False
+                my_writer.writerow(row.values())
+
+    def import_fields_from_csv(self, database_name, filename):
+        fields = []
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                fields.append(row)
+        return self.update_fields(database_name, fields)
+
+    def update_fields(self, database_name, fields):
+        output = []
+        for f in fields:
+            output.append(output.append(self.update_field(database_name, f)))
+        return output
+
+    def update_field(self, database_name, field):
+        field_from_api = self.field_tablenameandfieldname2field(database_name, field['table_name'], field['field_name'])
+        fk = self.field_tablenameandfieldname2field(database_name, field['foreign_table'], field['foreign_field'])
+        field.pop('foreign_table')
+        field.pop('foreign_field')
+        data = {'id': str(field_from_api['id'])}
+        for k in field.keys():
+            if field[k]:
+                data[k] = field[k]
+            else:
+                data[k] = None
+        if fk :
+            data['fk_target_field_id'] = fk['id']
+        return self.query('PUT', 'field/'+data['id'], data)
+            
 
 metabase = Metabase(metabase_apiurl, metabase_username, metabase_password)
 #metabase.debug = True
 #metabase.create_database('base', 'sqlite', '/path/to/db.sqlite')
 #metabase.delete_database('base')
-#print(json.dumps(metabase.get_field('base', 'table', 'field')))
 
-export = metabase.export_fields('igpvaucluse')
-with open('metabase_api.csv', 'w', newline = '') as csvfile:
-    my_writer = csv.writer(csvfile, delimiter = ';')
-    need_header = True
-    for row in export:
-        if need_header:
-            my_writer.writerow(row.keys())
-            need_header = False
-        my_writer.writerow(row.values())
+#print(json.dumps(metabase.get_field('base', 'table', 'Field name')))
+
+metabase.import_fields_from_csv(metabase_base, 'metabase_api_tobeimported.csv')
+metabase.export_fields_to_csv(metabase_base, 'metabase_api_exported.csv')
