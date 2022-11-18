25fbb4fbcb321328d237fb4a4be7afc36242140e
Tangui Morlier <tangui@tangui.eu.org>
2022-11-18 20:14:47 +0100

    Répertoire d'export configurable

--- metabase_export.py
+++ metabase_export.py
@@ -6,19 +6,17 @@ metabase_apiurl = sys.argv[1]
 metabase_username = sys.argv[2]
 metabase_password = sys.argv[3]
 metabase_base = sys.argv[4]
+metabase_exportdir = sys.argv[5]
 
 ametabase = metabase.MetabaseApi(metabase_apiurl, metabase_username, metabase_password)
 #ametabase.debug = True
 
-#ametabase.delete_database('base')
-#ametabase.create_database('base', 'sqlite', '/path/to/db.sqlite')
-
 try:
-    os.mkdir("export_"+metabase_base)
+    os.mkdir("export")
 except:
     None
 
-ametabase.export_fields_to_csv(metabase_base, "export_"+metabase_base)
-ametabase.export_cards_to_json(metabase_base, "export_"+metabase_base)
-ametabase.export_dashboards_to_json(metabase_base, "export_"+metabase_base)
-ametabase.export_metrics_to_json(metabase_base, "export_"+metabase_base)
+ametabase.export_fields_to_csv(metabase_base, metabase_exportdir)
+ametabase.export_cards_to_json(metabase_base, metabase_exportdir)
+ametabase.export_dashboards_to_json(metabase_base, metabase_exportdir)
+ametabase.export_metrics_to_json(metabase_base, metabase_exportdir)
--- metabase_import.py
+++ metabase_import.py
@@ -5,14 +5,12 @@ metabase_apiurl = sys.argv[1]
 metabase_username = sys.argv[2]
 metabase_password = sys.argv[3]
 metabase_base = sys.argv[4]
+metabase_exportdir = sys.argv[5]
 
 ametabase = metabase.MetabaseApi(metabase_apiurl, metabase_username, metabase_password)
 #ametabase.debug = True
 
-#ametabase.delete_database('base')
-#ametabase.create_database('base', 'sqlite', '/path/to/db.sqlite')
-
-ametabase.import_fields_from_csv(metabase_base, "export_"+metabase_base)
-ametabase.import_metrics_from_json(metabase_base, "export_"+metabase_base)
-ametabase.import_cards_from_json(metabase_base, "export_"+metabase_base)
-ametabase.import_dashboards_from_json(metabase_base, "export_"+metabase_base)
+ametabase.import_fields_from_csv(metabase_base, metabase_exportdir)
+ametabase.import_metrics_from_json(metabase_base, metabase_exportdir)
+ametabase.import_cards_from_json(metabase_base, metabase_exportdir)
+ametabase.import_dashboards_from_json(metabase_base, metabase_exportdir)
